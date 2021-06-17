9a39a6412d4aed3c37049aff215ef1f5ef854d61
Dawei Wei <davwei@microsoft.com>
2021-06-17 02:20:25 -0700

    Add monokai-theme and company package

--- emacs/init.el
+++ emacs/init.el
@@ -4,24 +4,35 @@
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
-(electric-indent-mode -1)   ;
 (global-linum-mode t)       ; show line number
-
 (menu-bar-mode -1)          ; Disable the menu bar
+(delete-selection-mode t)
 
 (setq make-backup-files nil); disable the backup file
+;; Set Up the visible bell
+(setq visible-bell t)
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(require 'recentf)
+(setq recentf-max-menu-items 25)
 
 ;; Quick open the emacs init file
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
+(load-theme 'monokai t)
+;;(setq monokai-background "#1F1F1F")
+(set-background-color "#1B1D1F")
 
-;; Set Up the visible bell
-(setq visible-bell t)
+;; highlight the parenparence
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+(global-hl-line-mode t)
 
-(load-theme 'wombat)
-
+(global-company-mode t)
+ 
 ;; Initialize package sources
 (require 'package)
 
@@ -31,7 +42,7 @@
 
 (package-initialize)
 (unless package-archive-contents
- (package-refresh-contents))
+   (package-refresh-contents))
 
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
@@ -44,7 +55,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages '(monokai-theme company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
